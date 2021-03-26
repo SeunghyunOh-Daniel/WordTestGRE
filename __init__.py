193256e2a984f8f351cc32b7c00672c9cb075c6a
SeunghyunOh-Daniel <seunghyun.daniel.oh@gmail.com>
2021-03-26 15:09:47 +0900

    file process

--- __init__.py
+++ __init__.py
@@ -1,26 +1,83 @@
-
-class Node:
-    def __init__(self, day, numbering, name, mean):
-        self.__day = day
-        self.__numbering = numbering
-        self.__name = name
-        self.__mean = mean
-    def __getattr__(self):
-        return [self.__day, self.__numbering, self.__name, self.__mean]
-    def __setattr__(self, day, numbering, name, mean):
-        self.__day = day
-        self.__numbering = numbering
-        self.__name = name
-        self.__mean = mean
+"""
+Compare a list of face encodings against a candidate encoding to see if they match.
+:param known_face_encodings: A list of known face encodings
+:param face_encoding_to_check: A single face encoding to compare against the list
+:param tolerance: How much distance between faces to consider it a match. Lower is more strict. 0.6 is typical best performance.
+:return: A list of True/False values indicating which known_face_encodings match the face encoding to check
+"""
 
 
-class Word(Node):
-    def __init__(self, index, day, numbering, name, mean):
-        super().__init__(day, numbering, name, mean)
-        self.__index = index
-    def __getattr__(self):
-        return [self.__index] + super().__getattr__()
+class Node(object):
+    def __init__(self, numbering, name, mean):
+        if numbering == 0:
+            raise ValueError(f"numbering have to be above 0")
+        self.numbering = numbering
+        self.name = name
+        self.mean = mean
 
-    def __setattr__(self, index, day, numbering, name, mean):
-        self.__index = index
-        self.__setattr__(day, numbering, name, mean)
+    def __get__(self):
+        return self.numbering, self.name, self.mean
+
+    def __set__(self, numbering=0, name=0, mean=0):
+        if not (numbering == 0):
+            self.numbering = numbering
+        if not (name == 0):
+            self.name = name
+        if not (mean == 0):
+            self.mean = mean
+
+
+class Words(object):
+    def __init__(self, day):
+        self.wordlist = []
+        # How to manage list only has the Node structure?
+        self.day = day
+
+    def __get__(self):
+        return self.day, self.wordlist
+
+    def __set__(self, day, wordlist):
+        self.day = day
+        self.wordlist = wordlist
+
+    def edit(self, numbering, name, mean):
+        if self.wordlist == []:
+            print("no wordlist in words class")
+            return 0
+        else:
+            for node in self.wordlist:
+                if node.numbering == numbering and node.name == name:
+                    node.mean = mean
+                    break
+            return 1
+
+    def print_test(self):
+        if self.wordlist == []:
+            print("no wordlist in words class")
+            return 0
+        for node in self.wordlist:
+            print(f"DAY{node.day}, {node.numbering}, {node.name}")
+        return 1
+
+    def print_answer(self):
+        if self.wordlist == []:
+            print("no wordlist in words class")
+            return 0
+        for node in self.wordlist:
+            print(f"DAY{self.day}, {node.numbering}, {node.name} -> {node.mean}")
+        return 1
+
+
+'''
+To need Edit
+    Word List
+    1 day Words - Node .... Node
+    2 day Words - Node .... Node
+    ...
+    3 day Words - Node .... Node
+    
+    Node
+        numbering
+        name
+        mean
+'''
--- main.py
+++ main.py
@@ -12,314 +12,360 @@ data_name = "1.xlsx"
 data_sheet = "표제어 list"
 data_result = "result/result.txt"
 
+
 def search_mean(word):
-	# Ref. https://greeksharifa.github.io/references/2020/10/30/python-selenium-usage/
+    # Ref. https://greeksharifa.github.io/references/2020/10/30/python-selenium-usage/
 
-	urlRef = ['https://translate.google.co.kr/?sl=en&tl=ko&text=', '&op=translate']
-	url = urlRef[0]+str(word)+urlRef[1]
-	print(url)
-	# initiating the webdriver. Parameter includes the path of the webdriver.
-	path_driver = './'
-	if "chromedriver.exe" not in os.listdir(path_driver):
-		print("error: install chromedriver")
-		return 0
-	driver = webdriver.Chrome('./chromedriver')
-	driver.get(url)
+    urlRef = ['https://translate.google.co.kr/?sl=en&tl=ko&text=', '&op=translate']
+    url = urlRef[0] + str(word) + urlRef[1]
+    print(url)
+    # initiating the webdriver. Parameter includes the path of the webdriver.
+    path_driver = './'
+    if "chromedriver.exe" not in os.listdir(path_driver):
+        print("error: install chromedriver")
+        return 0
+    driver = webdriver.Chrome('./chromedriver')
+    driver.get(url)
 
-	# if you search about dynamic driver, then you have to wait 'loading'
-	# this is just to ensure that the page is loaded
-	time.sleep(5)
+    # if you search about dynamic driver, then you have to wait 'loading'
+    # this is just to ensure that the page is loaded
+    time.sleep(5)
 
-	html = driver.page_source
+    html = driver.page_source
 
-	# this renders the JS code and stores all
-	# of the information in static HTML code.
+    # this renders the JS code and stores all
+    # of the information in static HTML code.
 
-	# Now, we could simply apply bs4 to html variable, html.parser or lxml
-	soup = BeautifulSoup(html, "lxml")
-	all_divs = soup.find('div', class_ = 'fw3eif')
-	try:
-		return all_divs.text
-	except AttributeError:
-		return 0
+    # Now, we could simply apply bs4 to html variable, html.parser or lxml
+    soup = BeautifulSoup(html, "lxml")
+    all_divs = soup.find('div', class_='fw3eif')
+    try:
+        return all_divs.text
+    except AttributeError:
+        return 0
+
 
 def enterDay():
-	day = input("Enter which day: ")
-	try:
-		if day == "exit":
-			return "exit"
-		if((int(day) > 30) or (int(day) < 0)):
-			return 0
-		else:
-			return int(day)
-	except ValueError:
-		return 0
+    day = input("Enter which day: ")
+    try:
+        if day == "exit":
+            return "exit"
+        if ((int(day) > 30) or (int(day) < 0)):
+            return 0
+        else:
+            return int(day)
+    except ValueError:
+        return 0
+
 
 def enterType():
-	print(f"1. New word 2. Wrong word 3.Research word(Preparing Service)")
-	print(f"---------------------------------------------")
-	answer = input("Enter which type: ")
-	print(f"---------------------------------------------")
-	try:
-		if answer == "exit":
-			return "exit"
-		if (int(answer) == 1) or (int(answer) == 2) or (int(answer)==3):
-			return int(answer)
-		else:
-			return 0
-	except ValueError:
-		return 0
+    print(f"1. New word 2. Wrong word 3.Research word(Preparing Service)")
+    print(f"---------------------------------------------")
+    answer = input("Enter which type: ")
+    print(f"---------------------------------------------")
+    try:
+        if answer == "exit":
+            return "exit"
+        if (int(answer) == 1) or (int(answer) == 2) or (int(answer) == 3):
+            return int(answer)
+        else:
+            return 0
+    except ValueError:
+        return 0
+
 
 def makeProblem_txt(data, day):
-	numRandom = np.random.randint(1, 102, 30)
-	problem_list = []
-	for index, dayLocal, numberofword, word, meaning  in zip(data["index"], data["day"], data["numberOfWord"], data["word"], data["mean"]):
-		if(day == dayLocal):
-			if(numberofword in numRandom):
-				problem_list.append([dayLocal, word, meaning])
-	return problem_list
+    numRandom = np.random.randint(1, 102, 30)
+    problem_list = []
 
-def processtxt_v2(data):
-	data = data.split('\n')
-	for i in range(len(data)-1):
-		data[i] = data[i].split(',')
+    for word in data:
+        index, day = word.getattr()
+        print(index, day)
+    # for index, dayLocal, numberofword, word, meaning in zip(data[index_day].__getattr__()):
+    # 	if(numberofword in numRandom):
+    # 		problem_list.append(data)
+    return problem_list
 
-	wordlist = []
-	for i in range(len(data)-1):
-		#index, day, numbering, name, mean):
-		word = Word(int(data[i][0]), int(data[i][1]), int(data[i][2]), data[i][3], data[i][4])
-		wordlist.append(word)
-	for i in range(len(wordlist)):
-		# -ing
-		print(i, wordlist[i].__getattr__())
-	return wordlist
+
+def process_txt_v2(data):
+    data = data.split('\n')
+    for i in range(len(data) - 1):
+        data[i] = data[i].split(',')[1:]
+    data = data[:-2]
+
+    # Day categorizing
+    # {day(type: int): word(number, name, mean), ...}
+    data_edit = {}
+    for value_list in data:
+        if not int(value_list[0].replace(" ", "")) in data_edit.keys():
+            data_edit[int(value_list[0])] = value_list[1:]
+        else:
+            data_edit[int(value_list[0])].append(value_list[1:])
+
+    word_list = []
+    # for value_list in data:
+    # 	print(value_list)
+    # 	word = Words(int(data[i][0]), int(data[i][1]), int(data[i][2]), data[i][3], data[i][4])
+    # 	wordlist.append(word)
+    # [ Word(), Word() ... , Word() ]
+
+    # Debug
+    # for i in range(len(wordlist)):
+    # 	index, day, numbering, name, mean = wordlist[i].__getattr__()
+    # 	print(index, day, numbering, name, mean)
+
+    return wordlist
+
 
 def saveWord(data_dict, problem_list, count):
-	data_dict[count] = problem_list
-	data_dict[count].append(1)
+    data_dict[count] = problem_list
+    data_dict[count].append(1)
+
 
 def printProblem(data_list):
-	print(f"--------QUIZ, DAY {data_list[0][0]}--------")
-	for val in data_list:
-		print(val[1])
-	print(f"---------------------------------------------")
-	while(True):
-		ans = input("YOU KNOW ALL?(y or n): ")
-		if (ans=="y")|(ans=="n"):break
-		if (ans=="exit"):break
-		else:
-			print("error: wrong input")
+    print(f"--------QUIZ, DAY {data_list[0][0]}--------")
+    for val in data_list:
+        print(val[1])
+    print(f"---------------------------------------------")
+    while (True):
+        ans = input("YOU KNOW ALL?(y or n): ")
+        if (ans == "y") | (ans == "n"): break
+        if (ans == "exit"):
+            break
+        else:
+            print("error: wrong input")
 
-	return ans
+    return ans
+
 
 def processtxt(data):
-	data = data.split('\n')
-	for i in range(len(data)-1):
-		data[i] = data[i].split(',')
+    data = data.split('\n')
+    for i in range(len(data) - 1):
+        data[i] = data[i].split(',')
 
-	data_dict = {}
-	data_dict["index"] = []
-	data_dict["day"] = []
-	data_dict["numberOfWord"] = []
-	data_dict["word"] = []
-	data_dict["mean"] = []
+    data_dict = {}
+    data_dict["index"] = []
+    data_dict["day"] = []
+    data_dict["numberOfWord"] = []
+    data_dict["word"] = []
+    data_dict["mean"] = []
 
-	for i in range(len(data)-1):
-		data_dict["index"].append(int(data[i][0]))
-		data_dict["day"].append(int(data[i][1]))
-		data_dict["numberOfWord"].append(int(data[i][2]))
-		data_dict["word"].append(data[i][3])
-		data_dict["mean"].append(data[i][4])
-	return data_dict
+    for i in range(len(data) - 1):
+        data_dict["index"].append(int(data[i][0]))
+        data_dict["day"].append(int(data[i][1]))
+        data_dict["numberOfWord"].append(int(data[i][2]))
+        data_dict["word"].append(data[i][3])
+        data_dict["mean"].append(data[i][4])
+    return data_dict
+
 
 def saveWord(data_dict, problem_list, count):
-	data_dict[count] = problem_list
-	data_dict[count].append(1)
+    data_dict[count] = problem_list
+    data_dict[count].append(1)
+
 
 def printProblem(data_list):
-	print(f"--------QUIZ, DAY {data_list[0][0]}--------")
-	for val in data_list:
-		print(val[1])
-	print(f"---------------------------------------------")
-	while(True):
-		ans = input("YOU KNOW ALL?(y or n): ")
-		if (ans=="y")|(ans=="n"):break
-		if (ans=="exit"):break
-		else:
-			print("error: wrong input")
+    print(f"--------QUIZ, DAY {data_list[0][0]}--------")
+    for val in data_list:
+        print(val[1])
+    print(f"---------------------------------------------")
+    while (True):
+        ans = input("YOU KNOW ALL?(y or n): ")
+        if (ans == "y") | (ans == "n"): break
+        if (ans == "exit"):
+            break
+        else:
+            print("error: wrong input")
 
-	return ans
+    return ans
+
 
 def printNonSolvedWord(data_dict):
+    if (len(data_dict) == 0):
+        return 0
+    elif (len(data_dict) == 1):
+        pNumber = 1
+    else:
+        pNumber = np.random.randint(1, len(data_dict))
 
-	if(len(data_dict)==0):
-		return 0
-	elif(len(data_dict)==1):pNumber = 1
-	else:
-		pNumber = np.random.randint(1, len(data_dict))
+    data_list = data_dict[pNumber]
 
-	data_list = data_dict[pNumber]
+    print(f"--------QUIZ, DAY {data_list[0][0]}--------")
+    for val in data_list:
+        if (type(val) == int): continue
+        print(val[1])
+    print(f"---------------------------------------------")
 
-	print(f"--------QUIZ, DAY {data_list[0][0]}--------")
-	for val in data_list:
-		if(type(val)==int):continue
-		print(val[1])
-	print(f"---------------------------------------------")
+    while (True):
+        ans = input("YOU KNOW ALL?(y or n): ")
+        if (ans == "y") | (ans == "n"): break
+        if (ans == "exit"):
+            break
+        else:
+            print("error: wrong input")
 
-	while(True):
-		ans = input("YOU KNOW ALL?(y or n): ")
-		if (ans=="y")|(ans=="n"):break
-		if (ans=="exit"):break
-		else:
-			print("error: wrong input")
+    return [ans, pNumber]
 
-	return [ans, pNumber]
 
 def printAnswer(data_list):
-	print(f"--------Answer, DAY {data_list[0][0]}--------")
-	for val in data_list:
-		if(type(val) == int):continue
-		print(f"{val[1]} -> {val[2]}")
-	print(f"---------------------------------------------")
+    print(f"--------Answer, DAY {data_list[0][0]}--------")
+    for val in data_list:
+        if (type(val) == int): continue
+        print(f"{val[1]} -> {val[2]}")
+    print(f"---------------------------------------------")
+
 
 def addWeightWord(data_dict, index):
-	data_dict[index][-1] = data_dict[index][-1]+1
+    data_dict[index][-1] = data_dict[index][-1] + 1
+
 
 def removeWord(data_dict, index):
-	del data_dict[index]
+    del data_dict[index]
+
 
 def subtractWeightWord(nonSolvedWord, index):
-	if(nonSolvedWord[index][-1] == 1):removeWord(nonSolvedWord, index)
-	else:
-		nonSolvedWord[index][-1] = nonSolvedWord[index][-1] - 1
+    if (nonSolvedWord[index][-1] == 1):
+        removeWord(nonSolvedWord, index)
+    else:
+        nonSolvedWord[index][-1] = nonSolvedWord[index][-1] - 1
+
 
 def edit_answer(nonSolvedWord):
-	print(f"--------------Problem List---------------")
-	for index, val in enumerate(nonSolvedWord.keys()):
-		print(f"{index+1}. DAY {nonSolvedWord[val][0][0]}")
-	print(f"-----------------------------------------")
+    print(f"--------------Problem List---------------")
+    for index, val in enumerate(nonSolvedWord.keys()):
+        print(f"{index + 1}. DAY {nonSolvedWord[val][0][0]}")
+    print(f"-----------------------------------------")
 
-	wordtoedit = input("Which number do you want?")
-	# Need to make exceptional case(if enter out of list)
-	index_words = int(wordtoedit)-1
-	index_words = list(nonSolvedWord.keys())[index]
-	# printAnswer(nonSolvedWord[index])
-	print(nonSolvedWord[index_words])
-	while(True):
-		print(f"-------------------------------------")
-		wordtoedit = input("What do you want to edit? ")
-		indexEdit = -1
-		for index, val in enumerate(nonSolvedWord[index_words][:-1]):
-		 	if(val[1].replace(" ","")==wordtoedit):indexEdit=index
-		if(indexEdit == -1):
-			print("no Word in list...Try again?(1)")
-			answer = input()
-			if(int(answer) == 1):continue
-			else:break
-		print(f"-------------------------------------")
-		print(f"{nonSolvedWord[index_words][indexEdit][1]} -> {nonSolvedWord[index_words][indexEdit][2]}")
-		print(f"-------------------------------------")
-		print("Mean you want to change:")
-		meantoedit = input()
-		print(f"-------------------------------------")
-		nonSolvedWord[index_words][indexEdit][2] = meantoedit
-		# -ing
-		print(nonSolvedWord[index_words][indexEdit])
-		print("Want a more editing? 1(yes) or 0(no)")
-		answer = input()
-		if((int(answer) == 0)):break
+    wordtoedit = input("Which number do you want?")
+    # Need to make exceptional case(if enter out of list)
+    index_words = int(wordtoedit) - 1
+    index_words = list(nonSolvedWord.keys())[index]
+    # printAnswer(nonSolvedWord[index])
+    print(nonSolvedWord[index_words])
+    while (True):
+        print(f"-------------------------------------")
+        wordtoedit = input("What do you want to edit? ")
+        indexEdit = -1
+        for index, val in enumerate(nonSolvedWord[index_words][:-1]):
+            if (val[1].replace(" ", "") == wordtoedit): indexEdit = index
+        if (indexEdit == -1):
+            print("no Word in list...Try again?(1)")
+            answer = input()
+            if (int(answer) == 1):
+                continue
+            else:
+                break
+        print(f"-------------------------------------")
+        print(f"{nonSolvedWord[index_words][indexEdit][1]} -> {nonSolvedWord[index_words][indexEdit][2]}")
+        print(f"-------------------------------------")
+        print("Mean you want to change:")
+        meantoedit = input()
+        print(f"-------------------------------------")
+        nonSolvedWord[index_words][indexEdit][2] = meantoedit
+        # -ing
+        print(nonSolvedWord[index_words][indexEdit])
+        print("Want a more editing? 1(yes) or 0(no)")
+        answer = input()
+        if ((int(answer) == 0)): break
+
 
 def main():
-	# https://realpython.com/python-timer/
-	# tic = time.perf_counter()
+    # https://realpython.com/python-timer/
+    # tic = time.perf_counter()
 
-	nonSolvedWord = {}
-	count = 1
-	f = open("./1.txt",  encoding='utf-8')
-	data = processtxt_v2(f.read())
+    nonSolvedWord = {}
+    nonSolvedWord_v2 = []
+    count = 1
+    f = open("./1.txt", encoding='utf-8')
+    data = process_txt_v2(f.read())
 
-	# while(True):
-	# 	print(f"---------------------------------------------")
-	# 	print(f"|		 GRE WORD TEST	 	    |")
-	# 	print(f"---------------------------------------------")
-	#
-	# 	problemType = enterType()
-	#
-	# 	if (problemType == "exit"): break
-	# 	if (problemType == "cls" or problemType == "clear"):os.system("cls")
-	#
-	# 	if problemType == 1:
-	#
-	# 		if len(nonSolvedWord) > 10:
-	# 			print(f"Please solve the probelm...")
-	# 			problemType = 2
-	#
-	# 		day = enterDay()
-	#
-	# 		if (day == "exit"): break
-	# 		if (problemType == "cls" or problemType == "clear"):os.system("cls")
-	#
-	# 		elif not((day > 0) and (day <= 30)):
-	# 			print(f"error: enter another day...day is from 1 to 30....")
-	#
-	# 		else:
-	# 			problem_list = makeProblem_txt(data, day)
-	# 			answer = printProblem(problem_list)
-	# 			answer = str(answer)
-	# 			if(answer=='exit'):break
-	# 			if(answer=='y'):
-	# 				continue
-	# 			elif(answer=='n'):
-	# 				saveWord(nonSolvedWord, problem_list, count)
-	# 				printAnswer(problem_list)
-	# 				count = count+1
-	#
-	# 	elif problemType == 2:
-	#
-	# 		if len(nonSolvedWord) < 1:
-	# 			print(f"NON PROBLEM: GREAT JOB!!!")
-	# 			continue
-	#
-	# 		print("HELLO REVIEW WORLD")
-	#
-	# 		ans_list = printNonSolvedWord(nonSolvedWord)
-	# 		answer = ans_list[0]
-	# 		index = ans_list[1]
-	# 		answer = str(answer)
-	# 		if (answer == 'exit'): break
-	# 		elif (answer == 'y'):
-	# 			subtractWeightWord(nonSolvedWord, index)
-	# 			continue
-	# 		elif (answer == 'n'):
-	# 			addWeightWord(nonSolvedWord, index)
-	# 			printAnswer(nonSolvedWord[index])
-	#
-	# 	elif problemType == 3:
-	# 		word = input("Which word do you want to search: ")
-	# 		print(f"The meaning...{search_mean(word)}")
-	# 	else:
-	# 		print("*error: check the exception in source")
-	#
-	# 	while(True):
-	# 		ansNextStep = input("Next Step?(y or n): ")
-	# 		if(ansNextStep == "exit"):break
-	# 		elif(ansNextStep == "cls" or problemType == "clear"):os.system("cls")
-	# 		elif(ansNextStep == "y"):break
-	# 		elif(ansNextStep == "n"):
-	# 			ansNextStep = input("What do you want to do?(1. edit the answer 2. next step)")
-	# 		if(ansNextStep == "2"):break
-	# 		elif(ansNextStep == "1"):edit_answer(nonSolvedWord)
-	# 		print("We are Waiting for U... If you want to next step, then press y")
-	#
-	# 	if(ansNextStep == "cls" or problemType == "clear"):os.system("cls")
-	# 	if(ansNextStep == "exit"):break
-	#
-	#
-	# # toc = time.perf_counter()
-	# # print(f"Process time in {toc - tic:0.4f} secondes")
 
-if __name__=="__main__":
-	main()
+# while(True):
+# 	print(f"---------------------------------------------")
+# 	print(f"|		 GRE WORD TEST	 	    |")
+# 	print(f"---------------------------------------------")
+#
+# 	problemType = enterType()
+#
+# 	if (problemType == "exit"): break
+# 	if (problemType == "cls" or problemType == "clear"):os.system("cls")
+#
+# 	if problemType == 1:
+#
+# 		if len(nonSolvedWord) > 10:
+# 			print(f"Please solve the probelm...")
+# 			problemType = 2
+#
+# 		day = enterDay()
+#
+# 		# Exit command
+# 		if (day == "exit"): break
+#
+# 		# Clear command
+# 		if (problemType == "cls" or problemType == "clear"):os.system("cls")
+#
+# 		# Exception Error for day 1-30
+# 		elif not((day > 0) and (day <= 30)):
+# 			print(f"error: enter another day...day is from 1 to 30....")
+#
+# 		# Execute
+# 		else:
+# 			problem_list = makeProblem_txt(data, day)
+# 			answer = printProblem(problem_list)
+# 			answer = str(answer)
+# 			if(answer=='exit'):break
+# 			if(answer=='y'):continue
+# 			elif(answer=='n'):
+# 				saveWord(nonSolvedWord, problem_list, count)
+# 				printAnswer(problem_list)
+# 				count = count+1
+#
+# 	elif problemType == 2:
+#
+# 		if len(nonSolvedWord) < 1:
+# 			print(f"NON PROBLEM: GREAT JOB!!!")
+# 			continue
+#
+# 		print("HELLO REVIEW WORLD")
+#
+# 		ans_list = printNonSolvedWord(nonSolvedWord)
+# 		answer = ans_list[0]
+# 		index = ans_list[1]
+# 		answer = str(answer)
+# 		if (answer == 'exit'): break
+# 		elif (answer == 'y'):
+# 			subtractWeightWord(nonSolvedWord, index)
+# 			continue
+# 		elif (answer == 'n'):
+# 			addWeightWord(nonSolvedWord, index)
+# 			printAnswer(nonSolvedWord[index])
+#
+# 	elif problemType == 3:
+# 		word = input("Which word do you want to search: ")
+# 		print(f"The meaning...{search_mean(word)}")
+# 	else:
+# 		print("*error: check the exception in source")
+#
+# 	while(True):
+# 		ansNextStep = input("Next Step?(y or n): ")
+# 		if(ansNextStep == "exit"):break
+# 		elif(ansNextStep == "cls" or problemType == "clear"):os.system("cls")
+# 		elif(ansNextStep == "y"):break
+# 		elif(ansNextStep == "n"):
+# 			ansNextStep = input("What do you want to do?(1. edit the answer 2. next step)")
+# 		if(ansNextStep == "2"):break
+# 		elif(ansNextStep == "1"):edit_answer(nonSolvedWord)
+# 		print("We are Waiting for U... If you want to next step, then press y")
+#
+# 	if(ansNextStep == "cls" or problemType == "clear"):os.system("cls")
+# 	if(ansNextStep == "exit"):break
+#
+#
+# # toc = time.perf_counter()
+# # print(f"Process time in {toc - tic:0.4f} secondes")
+
+if __name__ == "__main__":
+    main()
 
 # Weight
 # Sorting word list
